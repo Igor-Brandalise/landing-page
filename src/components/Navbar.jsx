33a5c0450cd3aabeed2b04bdfea41a658e80df0b
Igor Brandalise <brandaliseigor123@gmail.com>
2025-04-21 00:33:01 -0300

    ajuste navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,37 @@
 function Navbar() {
-    return (
-      <nav className="bg-gradient-to-r from-[#D53469] via-[#D76E5D] to-[#D9A552]">
+  return (
 
+    <nav className="bg-gradient-to-r from-[#D53469] via-[#D76E5D] to-[#D9A552]">
+      <div className="flex flex-col items-center md:flex-row md:justify-between w-full px-4 py-4">
 
-<div className="flex justify-between items-center w-full px-4 py-4">
+        {/* Logo */}
+        <h1 className="text-[30px] font-extrabold text-white xl:ml-10 mb-5 mr-5 md:mb-0 md:ml-10">
+          Landing
+        </h1>
 
-  
-  <h1 className="text-2xl font-extrabold text-white ml-10 ">Landing</h1>
+        {/* Menu + Botão */}
+        <div className="flex gap-5 items-center md:flex-row ">
+          <ul className="flex gap-6 text-black font-medium md:flex-row items-center">
+            <li>
+              <a href="#">link</a>
+            </li>
+            <li>
+              <a href="#">link</a>
+            </li>
+            <li>
+              <a href="#">link</a>
+            </li>
+          </ul>
 
+          <button className="bg-[#F5E8D3] h-[2.5em] w-[6em] rounded-3xl font-bold xl:mr-10">
+            Action
+          </button>
+        </div>
+      </div>
 
-  <div className="flex items-center gap-6">
-    <ul className="flex gap-6 text-black font-medium">
-      <li><a href="#">link</a></li>
-      <li><a href="#">link</a></li>
-      <li><a href="#">link</a></li>
-    </ul>
+      <hr className="h-[2px] w-full bg-[#eb9b99] border-0 rounded-full" />
+    </nav>
+  );
+}
 
-    <button className="bg-[#F5E8D3] h-[2.5em] w-[6em] rounded-3xl mr-10 font-bold">Action</button>
-  </div>
-</div>
-
-      <hr className="h-[2px] w-full bg-[#eb9b99] border-0 rounded-full"/> 
-      
-      </nav>
-      
-    );
-  }
-  
-  export default Navbar;
+export default Navbar;
